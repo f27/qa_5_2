c013540d1f48177eba6b1c4344f74f36ab331bd9
John Smoker <testgits@yandex.ru>
2021-03-17 03:46:49 +0500

    Separate basic test and with faker

--- src/test/java/tests/FormTests.java
+++ src/test/java/tests/FormTests.java
@@ -1,16 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.time.ZoneId;
-import java.time.format.TextStyle;
-import java.util.Locale;
-
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class FormTests {
     @BeforeAll
@@ -22,55 +18,41 @@ public class FormTests {
     void formFillTest() {
         open("https://demoqa.com/automation-practice-form");
 
-        Faker faker = new Faker();
-        String firstname = faker.name().firstName(),
-                lastname = faker.name().lastName(),
-                email = faker.internet().emailAddress(),
-                gender = faker.demographic().sex(),
-                mobile = faker.phoneNumber().subscriberNumber(10),
-                monthofbirth = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH),
-                yearofbirth = String.valueOf(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).getYear()),
-                dayofbirth = String.valueOf(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).getDayOfMonth()),
-                subject1 = "English",
-                subject2 = "Maths",
-                hobby1 = "Sports",
-                hobby2 = "Music",
-                picture = "cat.png",
-                address = faker.address().fullAddress(),
-                state = "NCR",
-                city = "Noida";
-
-        $("#firstName").setValue(firstname);
-        $("#lastName").setValue(lastname);
-        $("#userEmail").setValue(email);
-        $$("#genterWrapper label").findBy(text(gender)).click();
-        $("#userNumber").setValue(mobile);
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Testoff");
+        $("#userEmail").setValue("someemail@qaqaqaqa.qa");
+        $("[for='gender-radio-1']").click();
+        $("#userNumber").setValue("0123456789");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(monthofbirth);
-        $(".react-datepicker__year-select").selectOption(yearofbirth);
-        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text(dayofbirth)).click();
-        $("#subjectsInput").setValue(subject1).pressEnter();
-        $("#subjectsInput").setValue(subject2).pressEnter();
-        $$("#hobbiesWrapper label").findBy(text(hobby1)).click();
-        $$("#hobbiesWrapper label").findBy(text(hobby2)).click();
-        $("#uploadPicture").uploadFromClasspath(picture);
-        $("#currentAddress").setValue(address);
-        $("#react-select-3-input").setValue(state).pressEnter();
-        $("#react-select-4-input").setValue(city).pressEnter();
+        $(".react-datepicker__month-select").selectOptionByValue("2");
+        $(".react-datepicker__year-select").selectOptionByValue("2011");
+        $("[aria-label='Choose Monday, March 28th, 2011']").click();
+        $("#subjectsInput").setValue("English");
+        $("#subjectsInput").pressEnter();
+        $("#subjectsInput").setValue("Maths");
+        $("#subjectsInput").pressEnter();
+        $("[for='hobbies-checkbox-1']").click();
+        $("[for='hobbies-checkbox-3']").click();
+        $("#uploadPicture").uploadFromClasspath("cat.png");
+        $("#currentAddress").setValue("Moscow, Neverland");
+        $("#react-select-3-input").setValue("NCR");
+        $("#react-select-3-input").pressEnter();
+        $("#react-select-4-input").setValue("Noida");
+        $("#react-select-4-input").pressEnter();
         $("#submit").click();
 
-        $(".modal-content").shouldHave(text(firstname),
-                text(lastname),
-                text(email),
-                text(gender),
-                text(mobile),
-                text(dayofbirth+" "+monthofbirth+","+yearofbirth),
-                text(subject1+", "+subject2),
-                text(hobby1+", "+hobby2),
-                text(picture),
-                text(address),
-                text(state),
-                text(city));
+        $(".modal-content").shouldHave(text("Alex"),
+                text("Testoff"),
+                text("someemail@qaqaqaqa.qa"),
+                text("Male"),
+                text("0123456789"),
+                text("28 March,2011"),
+                text("English, Maths"),
+                text("Sports, Music"),
+                text("cat.png"),
+                text("Moscow, Neverland"),
+                text("NCR"),
+                text("Noida"));
 
 
     }

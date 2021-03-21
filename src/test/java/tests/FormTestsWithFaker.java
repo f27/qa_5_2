ca74c8dc6b50f55c9c6f275cb9691f0555fa6e34
John Smoker <testgits@yandex.ru>
2021-03-21 18:57:14 +0500

    Date with date format

--- src/test/java/tests/FormTestsWithFaker.java
+++ src/test/java/tests/FormTestsWithFaker.java
@@ -5,8 +5,7 @@ import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.time.ZoneId;
-import java.time.format.TextStyle;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static com.codeborne.selenide.Condition.text;
@@ -15,6 +14,41 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FormTestsWithFaker {
+    Faker faker = new Faker();
+    Date dateOfBirth = faker.date().birthday();
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy"),
+            monthFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH),
+            dayFormat = new SimpleDateFormat("dd");
+
+    static HashMap<String, String[]> statesAndCities = new HashMap<>();
+
+    static {
+        statesAndCities.put("NCR", new String[]{"Delhi", "Gurgaon", "Noida"});
+        statesAndCities.put("Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"});
+        statesAndCities.put("Haryana", new String[]{"Karnal", "Panipat"});
+        statesAndCities.put("Rajasthan", new String[]{"Jaipur", "Jaiselmer"});
+    }
+
+    List<String> statesList = new ArrayList<>(statesAndCities.keySet());
+
+    String firstname = faker.name().firstName(),
+            lastname = faker.name().lastName(),
+            email = faker.internet().emailAddress(firstname.toLowerCase() + "." + lastname.toLowerCase()),
+            gender = faker.demographic().sex(),
+            mobile = faker.numerify("##########"), //10 Digits
+            monthOfBirth = monthFormat.format(dateOfBirth),
+            yearOfBirth = yearFormat.format(dateOfBirth),
+            dayOfBirth = dayFormat.format(dateOfBirth),
+            picture = "cat.png",
+            address = faker.address().fullAddress(),
+            state = statesList.get(faker.random().nextInt(statesList.size())),
+            city = statesAndCities.get(state)[faker.random().nextInt(statesAndCities.get(state).length)],
+            formLabel = "Student Registration Form",
+            modalLabel = "Thanks for submitting the form";
+
+    String[] subjects = {"English", "Maths", "Arts", "Accounting"}, hobbies = {"Sports", "Music"};
+
+
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
@@ -22,37 +56,6 @@ public class FormTestsWithFaker {
 
     @Test
     void formFillTestWithFaker() {
-        Faker faker = new Faker();
-        Date dateOfBirth = faker.date().birthday();
-        Map<String, String[]> statesAndCities = new HashMap<>();
-        statesAndCities.put("NCR", new String[]{"Delhi", "Gurgaon", "Noida"});
-        statesAndCities.put("Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"});
-        statesAndCities.put("Haryana", new String[]{"Karnal", "Panipat"});
-        statesAndCities.put("Rajasthan", new String[]{"Jaipur", "Jaiselmer"});
-
-        List<String> statesList = new ArrayList<>(statesAndCities.keySet());
-
-        String firstname = faker.name().firstName(),
-                lastname = faker.name().lastName(),
-                email = faker.internet().emailAddress(firstname.toLowerCase() + "." + lastname.toLowerCase()),
-                gender = faker.demographic().sex(),
-                mobile = faker.numerify("##########"), //10 Digits
-                monthOfBirth = dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).getMonth().getDisplayName(TextStyle.FULL, Locale.ENGLISH),
-                yearOfBirth = String.valueOf(dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).getYear()),
-                dayOfBirth = String.valueOf(dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).getDayOfMonth()),
-                picture = "cat.png",
-                address = faker.address().fullAddress(),
-                state = statesList.get(faker.random().nextInt(statesList.size())),
-                city = statesAndCities.get(state)[faker.random().nextInt(statesAndCities.get(state).length)],
-                formLabel = "Student Registration Form",
-                modalLabel = "Thanks for submitting the form";
-
-        if (dayOfBirth.length() < 2) {
-            dayOfBirth = "0" + dayOfBirth;
-        }
-
-        String[] subjects = {"English", "Maths", "Arts", "Accounting"}, hobbies = {"Sports", "Music"};
-
         open("https://demoqa.com/automation-practice-form");
 
         $(".practice-form-wrapper").shouldHave(text(formLabel));
---
+++ src/test/java/tests/pageobjects/FormWithFakerTests.java
@@ -0,0 +1,4 @@
+package tests.pageobjects;
+
+public class FormWithFakerTests {
+}

609179b6422dadf167a3ad26354a896b290e7439
John Smoker <testgits@yandex.ru>
2021-03-23 19:13:40 +0500

    Added vars in chain

--- src/test/java/tests/pageobjects/chain/FormWithFakerAndJavaScriptPage.java
+++ src/test/java/tests/pageobjects/chain/FormWithFakerAndJavaScriptPage.java
@@ -1,29 +1,49 @@
 package tests.pageobjects.chain;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 
 import java.util.Map;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static utils.FileUtils.readStringFromFile;
 import static utils.JsonUtils.mapFromJson;
 
 public class FormWithFakerAndJavaScriptPage {
+    private static final SelenideElement
+            formTitleField = $(".practice-form-wrapper"),
+            firstNameField = $("#firstName"),
+            lastNameField = $("#lastName"),
+            emailField = $("#userEmail"),
+            genderField = $("#genterWrapper"),
+            mobileField = $("#userNumber"),
+            datePickerField = $("#dateOfBirthInput"),
+            yearField = $(".react-datepicker__year-select"),
+            monthField = $(".react-datepicker__month-select"),
+            subjectsInput = $("#subjectsInput"),
+            firstSubject = $("#react-select-2-option-0"),
+            hobbiesWrapper = $("#hobbiesWrapper"),
+            pictureUpload = $("#uploadPicture"),
+            addressField = $("#currentAddress"),
+            stateField = $("#state"),
+            cityField = $("#city"),
+            submitButton = $("#submit"),
+            modalWindow = $(".modal-content"),
+            closeModalButton = $("#closeLargeModal");
 
     public FormWithFakerAndJavaScriptPage fillForm(Map<String,String> userData) {
-        $(".practice-form-wrapper").shouldHave(text(userData.get("Form Title")));
-        $("#firstName").setValue(userData.get("First Name"));
-        $("#lastName").setValue(userData.get("Last Name"));
-        $("#userEmail").setValue(userData.get("Email"));
-        $("#genterWrapper").$(byText(userData.get("Gender"))).scrollIntoView(true).click();
-        $("#userNumber").setValue(userData.get("Mobile"));
-        $("#dateOfBirthInput").scrollIntoView(true).click();
+        formTitleField.shouldHave(text(userData.get("Form Title")));
+        firstNameField.setValue(userData.get("First Name"));
+        lastNameField.setValue(userData.get("Last Name"));
+        emailField.setValue(userData.get("Email"));
+        genderField.$(byText(userData.get("Gender"))).scrollIntoView(true).click();
+        mobileField.setValue(userData.get("Mobile"));
+        datePickerField.scrollIntoView(true).click();
         fillDatePicker(userData.get("Year Of Birth"), userData.get("Month Of Birth"), userData.get("Day Of Birth"));
         for (String subject : userData.get("Subjects").split(", ")) {
             addSubject(subject);
@@ -33,20 +53,20 @@ public class FormWithFakerAndJavaScriptPage {
             addHobby(hobby);
         }
         Configuration.clickViaJs = false;
-        $("#uploadPicture").uploadFromClasspath(userData.get("Picture"));
-        $("#currentAddress").setValue(userData.get("Address"));
-        $("#state").scrollIntoView(true).click();
-        $("#state").find(byText(userData.get("State"))).scrollIntoView(true).click();
-        $("#city").scrollIntoView(true).click();
-        $("#city").find(byText(userData.get("City"))).scrollIntoView(true).click();
-        $("#submit").scrollIntoView(true).click();
+        pictureUpload.uploadFromClasspath(userData.get("Picture"));
+        addressField.setValue(userData.get("Address"));
+        stateField.scrollIntoView(true).click();
+        stateField.find(byText(userData.get("State"))).scrollIntoView(true).click();
+        cityField.scrollIntoView(true).click();
+        cityField.find(byText(userData.get("City"))).scrollIntoView(true).click();
+        submitButton.scrollIntoView(true).click();
 
         return this;
     }
 
     public FormWithFakerAndJavaScriptPage closeModal() {
-        $("#closeLargeModal").scrollIntoView(true).click();
-        $(".modal-content").shouldNotBe(visible);
+        closeModalButton.scrollIntoView(true).click();
+        modalWindow.shouldNotBe(visible);
 
         return this;
     }
@@ -63,18 +83,18 @@ public class FormWithFakerAndJavaScriptPage {
     }
 
     private static void fillDatePicker(String year, String month, String day) {
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__year-select").selectOption(year);
+        monthField.selectOption(month);
+        yearField.selectOption(year);
         $(String.format(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)", day)).scrollIntoView(true).click();
     }
 
     private static void addSubject(String subject) {
-        $("#subjectsInput").setValue(subject);
-        $("#react-select-2-option-0").scrollIntoView(true).click();
+        subjectsInput.setValue(subject);
+        firstSubject.scrollIntoView(true).click();
     }
 
     private static void addHobby(String hobby) {
-        $("#hobbiesWrapper").$(byText(hobby)).scrollIntoView(true).click();
+        hobbiesWrapper.$(byText(hobby)).scrollIntoView(true).click();
     }
 
 }
